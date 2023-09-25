cfbf75a3b064c8543498fa0c074fcae7cf4c2965
Alex Carlson <alex@acwd.me>
2023-09-24 22:45:20 -0400

    added doors and lightweight/flexible interactable system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,14 @@ public class PlayerController : MonoBehaviour
     public Animator anim;
     public float moveSpeed;
     public float jumpHeight;
+    public float interactRange = 5;
     private float XAxis = 0;
     private bool jump = false;
     private Rigidbody _rb;
+    private bool isRight = true;
+    private bool canInteract = false;
+    private GameObject interactableObject;
+    private Collider[] interactCols;
 
     private void Start()
     {
@@ -20,6 +25,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessInput();
+        CheckInteractions();
+    }
+
+    private void FixedUpdate()
+    {
         Move();
         Jump();
     }
@@ -30,17 +40,74 @@ public class PlayerController : MonoBehaviour
         jump = Input.GetButtonDown("Jump");
     }
 
+    private void CheckInteractions()
+    {
+        interactCols = Physics.OverlapSphere(mover.position, interactRange);
+
+        if (interactCols.Length > 0)
+        {
+            foreach (Collider c in interactCols)
+            {
+                if (c.CompareTag("Interactable"))
+                {
+                    canInteract = true;
+                    SetInteractableObject(c);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            canInteract = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && canInteract)
+        {
+            interactableObject.GetComponent<InteractableObject>().Interact();
+        }
+    }
+
     private void Move()
     {
         float runAnimMultiplier = 1.2f;
-        anim.SetFloat("moveSpeed", Mathf.Abs(XAxis) * runAnimMultiplier);
+        anim.SetFloat("moveSpeed", _rb.velocity.normalized.magnitude * runAnimMultiplier);
         anim.SetFloat("jumpHeight", _rb.velocity.y);
         _rb.AddForce(new Vector3(0, 0, XAxis * moveSpeed));
-        anim.gameObject.transform.rotation = Quaternion.Euler(0, XAxis > 0 ? 0 : 180, 0);
+
+        if (XAxis > 0.1f)
+        {
+            isRight = true;
+        }
+        else if (XAxis < -0.1f)
+        {
+            isRight = false;
+        }
+
+        anim.gameObject.transform.rotation = Quaternion.Euler(0, isRight ? 0 : 180, 0);
     }
 
     private void Jump()
     {
+        if (jump && IsGrounded())
+        {
+            Debug.Log("Jump");
+            _rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+        }
+    }
 
+    public void Teleport(Transform newPosition)
+    {
+        mover.position = newPosition.position;
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 offset = new Vector3(0, 0.1f, 0);
+        return Physics.Raycast(mover.position + offset, -Vector3.up, 0.2f);
+    }
+
+    public void SetInteractableObject(Collider c)
+    {
+        interactableObject = c.gameObject;
     }
 }
