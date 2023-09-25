3a901009cd2296c678b32ecef8c177508c88a754
Alex Carlson <alex-carlson@users.noreply.github.com>
2023-09-24 22:03:58 -0400

    improve movement controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Transform mover;
+    public Animator anim;
     public float moveSpeed;
     public float jumpHeight;
     private float XAxis = 0;
     private bool jump = false;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = mover.GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -25,7 +32,11 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        mover.transform.Translate(new Vector3(0, 0, XAxis * moveSpeed * Time.deltaTime));
+        float runAnimMultiplier = 1.2f;
+        anim.SetFloat("moveSpeed", Mathf.Abs(XAxis) * runAnimMultiplier);
+        anim.SetFloat("jumpHeight", _rb.velocity.y);
+        _rb.AddForce(new Vector3(0, 0, XAxis * moveSpeed));
+        anim.gameObject.transform.rotation = Quaternion.Euler(0, XAxis > 0 ? 0 : 180, 0);
     }
 
     private void Jump()
